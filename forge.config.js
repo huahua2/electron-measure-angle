27b3c83ecb5b12faff0135a16d2b6d97f25cca49
huahua2 <lghua2009@163.com>
2023-01-29 21:48:34 +0800

    build: maker-dmg

--- forge.config.js
+++ forge.config.js
@@ -8,17 +8,26 @@ module.exports = {
     //   name: '@electron-forge/maker-squirrel',
     //   config: {},
     // },
-    {
-      name: '@electron-forge/maker-zip',
-      // platforms: ['darwin']
-    },
     // {
-    //   name: '@electron-forge/maker-dmg',
-    //   config: {
-    //     background: './assets/logo.png',
-    //     format: 'ULFO',
-    //     // icon: '/path/to/icon.icns',
-    //   }
-    // }
+      // name: '@electron-forge/maker-zip',
+      // platforms: ['darwin']
+    // },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: './assets/icon.icns',
+        overwrite: true,
+        additionalDMGOptions: {
+          window: {
+             size: {
+               width: 600,
+               height: 600
+             }
+          }
+        },
+        format: 'ULFO',
+        iconSize: 60
+      }
+    }
   ],
 };
